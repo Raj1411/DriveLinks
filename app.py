0cf4f34b942dd321107b69d4969f3b9522344030
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2021-09-14 21:23:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,12 +19,19 @@ else:
     gauth=GoogleAuth()
     gauth.LoadCredentialsFile("mycreds.txt")
     if gauth.credentials is None:
-        gauth.LocalWebserverAuth()
+            gauth.GetFlow()
+            gauth.flow.params.update({'access_type': 'offline'})
+            gauth.flow.params.update({'approval_prompt': 'force'})
+            gauth.LocalWebserverAuth()
     elif gauth.access_token_expired:
             gauth.Refresh()
     else:
         gauth.Authorize()
-    gauth.SaveCredentialsFile("mycreds.txt")
+    gauth.SaveCredentialsFile("mycreds.txt")  
+
+
+
+
 
 
     drive=GoogleDrive(gauth)
