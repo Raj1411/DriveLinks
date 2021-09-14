8087c7dc58a97eaa5547c27c8533947bcbf6ace0
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2021-09-14 20:38:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if file_id=='':
     'Please Enter File id '
 else:
     gauth=GoogleAuth()
-    gauth.LocalWebserverAuth()
+    gauth.LocalWebserverAuth(host_name='http://localhost:8080/')
     drive=GoogleDrive(gauth)
     keyboard.press_and_release('ctrl+w')
     per={
